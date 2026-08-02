aecd599ff5eb0930e9018df966236b3c5da19b13
Dilan Lakshitha <lakshithadilan6@gmail.com>
2026-08-02 12:05:23 +0530

    chh

--- src/app/sharedComponents/contact-us-component/contact-us-component.ts
+++ src/app/sharedComponents/contact-us-component/contact-us-component.ts
@@ -1,6 +1,6 @@
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, Inject, Input, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, Input, OnDestroy, PLATFORM_ID } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
@@ -15,16 +15,23 @@ import countryCode from './../../../assets/data/countryCode.json';
   templateUrl: './contact-us-component.html',
   styleUrls: ['./contact-us-component.css'],
 })
-export class ContactUsComponent {
-  @Input() homecontact: boolean = false;
+export class ContactUsComponent implements OnDestroy {
+  @Input() homecontact = false;
   contactForm: any;
   successMessage = '';
+  isSubmitting = false;
   countriesList = countryCode;
-  selectedCountryCode: string = 'LK'; 
-  phoneNumber:string = '';
-  private timeoutId: any = null;
+  selectedCountryCode = 'LK';
+  private timeoutId: ReturnType<typeof setTimeout> | null = null;
+  private readonly isBrowser: boolean;
 
-  constructor(private fb: FormBuilder, private http: HttpClient, private toastr:ToastrService , @Inject(PLATFORM_ID) private platformId: Object) {
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private toastr: ToastrService,
+    @Inject(PLATFORM_ID) platformId: Object,
+  ) {
+    this.isBrowser = isPlatformBrowser(platformId);
     this.contactForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -33,92 +40,56 @@ export class ContactUsComponent {
     });
   }
 
- onSubmit() {
-    if (!isPlatformBrowser(this.platformId)) {
+  onSubmit() {
+    if (!this.isBrowser || this.isSubmitting || this.contactForm.invalid) {
       return;
     }
 
-    if (this.contactForm.valid) {
-      const country = this.countriesList.find(
-        (c) => c.code === this.selectedCountryCode
-      );
+    const country = this.countriesList.find(
+      (c) => c.code === this.selectedCountryCode,
+    );
+    const fullPhoneNumber =
+      (country?.dial_code ?? '') + this.contactForm.get('contactPhone')?.value;
 
-      const fullPhoneNumber =
-        (country?.dial_code ?? '') +
-        this.contactForm.get('contactPhone')?.value;
+    const formData = {
+      ...this.contactForm.value,
+      contactPhone: fullPhoneNumber,
+    };
 
-      const formData = {
-        ...this.contactForm.value,
-        contactPhone: fullPhoneNumber,
-      };
-
-      this.http
-        .post(`${environment.backendUrl}/send-contact-email`, formData)
-        .subscribe({
-          next: (res: any) => {
-            this.successMessage = 'Your message has been sent successfully!';
-            this.toastr.success('Message sent successfully!');
-
-            this.contactForm.reset();
-
-            if (this.timeoutId) {
-              clearTimeout(this.timeoutId);
-            }
-
-            this.timeoutId = setTimeout(() => {
-              this.successMessage = '';
-            }, 3000);
-          },
-          error: (err) => {
-            console.error('Email error:', err);
-            this.successMessage =
-              'There was an error sending your message. Please try again later.';
-            this.toastr.error('Failed to send message');
-
-            if (this.timeoutId) {
-              clearTimeout(this.timeoutId);
-            }
-
-            this.timeoutId = setTimeout(() => {
-              this.successMessage = '';
-            }, 3000);
-          },
-        });
-    }
-  
-    if (this.contactForm.valid) {
-      console.log('Selected Country Code:', this.selectedCountryCode);
-      const country = this.countriesList.find(c => c.code === this.selectedCountryCode);
-      const fullPhoneNumber = (country?.dial_code ?? '') + this.contactForm.get('contactPhone')?.value;
-
-      const formData = {
-        ...this.contactForm.value,
-        contactPhone: fullPhoneNumber,
-      };
-
-      console.log('Form Data:', formData);
-
+    this.isSubmitting = true;
     this.http
       .post(`${environment.backendUrl}/send-contact-email`, formData)
       .subscribe({
-        next: (res: any) => {
-          console.log('Email sent successfully:', res);
+        next: () => {
+          this.isSubmitting = false;
           this.successMessage = 'Your message has been sent successfully!';
+          this.toastr.success('Message sent successfully!');
           this.contactForm.reset();
-
-          setTimeout(() => {
-            this.successMessage = '';
-          }, 3000);
+          this.clearSuccessLater();
         },
         error: (err) => {
+          this.isSubmitting = false;
           console.error('Email error:', err);
-          this.successMessage = 'There was an error sending your message. Please try again later.';
-
-          setTimeout(() => {
-            this.successMessage = '';
-          }, 3000);
+          this.successMessage =
+            'There was an error sending your message. Please try again later.';
+          this.toastr.error('Failed to send message');
+          this.clearSuccessLater();
         },
       });
+  }
+
+  ngOnDestroy() {
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
     }
+  }
+
+  private clearSuccessLater() {
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
+    }
+    this.timeoutId = setTimeout(() => {
+      this.successMessage = '';
+    }, 3000);
   }
 }
